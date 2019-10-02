0b1719012ff84297775aaddf6fe770b243bb2bd5
huiaic <huiaic@uw.edu>
2019-10-02 15:25:04 -0700

    cab-3746: update script

--- content-api-crud/scripts/writeIdToFile.js
+++ content-api-crud/scripts/writeIdToFile.js
@@ -1,7 +1,9 @@
 var writer = new java.io.FileWriter('./data/id.csv');
 writer.append('id\n');
-for (var i=1; i <= vars.get('totalCount'); i++) {
-  writer.append(vars.get('id_'+ i));
+var i = 1;
+while(vars.get('id_' + i) != null) {
+  writer.append(vars.get('id_' + i));
   writer.append('\n');
+  i++;
 }
 writer.close();
